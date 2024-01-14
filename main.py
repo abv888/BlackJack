d0a55a4fa03a977f6dccaf08ec689b46b193c38f
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2024-01-14 16:47:05 +0300

    game logic

--- main.py
+++ main.py
@@ -1,1 +1,77 @@
-print("Hello, World!")
+import random
+from replit import clear
+from art import logo
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+def compare_score(user_score, dealer_score):
+    if user_score == dealer_score:
+        return "Draw"
+    elif dealer_score == 0:
+        return "Lose, opponent has BlackJack"
+    elif user_score == 0:
+        return "Win the BlackJack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif dealer_score > 21:
+        return "Opponent went over. You win"
+    elif user_score > dealer_score:
+        return "You win"
+    else:
+        return "You win"
+
+
+def game():
+    print(logo)
+
+    user_cards = []
+    dealer_cards = []
+    user_score = 0
+    dealer_score = 0
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        dealer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        dealer_score = calculate_score(dealer_cards)
+        print(f"  Your cards: {user_cards}, current score: {user_score}")
+        print(f"  Dealer's first card: {dealer_cards[0]}")
+
+        if user_score == 0 or dealer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, or type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while dealer_score != 0 and dealer_score < 17:
+        dealer_cards.append(deal_card())
+        dealer_score = calculate_score(dealer_cards)
+
+    print(f"  Your final hand: {user_cards}, final score: {user_score}")
+    print(f"  Dealer's final hand: {dealer_cards}, final score: {dealer_score}")
+    print(compare_score(user_score, dealer_score))
+
+
+while input("Do you want to play a game of BlackJack? Type 'y' or 'n': ") == "y":
+    clear()
+    game()
